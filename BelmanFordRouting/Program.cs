d1802def02ae2d33e371e67064d2ff0311dd4447
abdurrahmanaksoy <abdurrahmanaksoy47@hotmail.com>
2018-12-13 22:03:28 +0300

    First Commit

--- BelmanFordRouting/Program.cs
+++ BelmanFordRouting/Program.cs
@@ -6,10 +6,78 @@ using System.Threading.Tasks;
 
 namespace BelmanFordRouting
 {
-    class Program
+    public class Path
     {
-        static void Main(string[] args)
+        public string From;
+        public string To;
+        public int Cost;
+
+        public Path(string from, string to, int cost)
         {
+            From = from;
+            To = to;
+            Cost = cost;
+        }
+    }
+    public class Program
+    {
+        public static string[] vertices = { "S", "A", "B", "C", "D", "E" };
+
+        public static List<Path> graph = new List<Path>()
+        {
+            new Path("S", "A", 1),
+            new Path("S", "E", 2),
+            new Path("A", "C", 3),
+            new Path("B", "A", 2),
+            new Path("C", "B", 1),
+            new Path("D", "C", 3),
+            new Path("D", "A", 1),
+            new Path("E", "D", 2)
+        };
+
+        public static Dictionary<string, int> memo = new Dictionary<string, int>()
+        {
+            { "S", 0 },
+            { "A", int.MaxValue },
+            { "B", int.MaxValue },
+            { "C", int.MaxValue },
+            { "D", int.MaxValue },
+            { "E", int.MaxValue }
+        };
+
+        public static bool Iterate()
+        {
+            bool doItAgain = false;
+
+            foreach (var fromVertex in vertices)
+            {
+                Path[] edges = graph.Where(x => x.From == fromVertex).ToArray();
+                foreach (var edge in edges)
+                {
+                    int potentialCost = memo[edge.From] == int.MaxValue ? int.MaxValue : memo[edge.From] + edge.Cost;
+                    if (potentialCost < memo[edge.To])
+                    {
+                        memo[edge.To] = potentialCost;
+                        doItAgain = true;
+                    }
+                }
+            }
+            return doItAgain;
+        }
+
+        static void Main()
+        {
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                if (!Iterate())
+                    break;
+            }
+
+            foreach (var keyValue in memo)
+            {
+                Console.WriteLine($"{keyValue.Key}: {keyValue.Value}");
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using BelmanFordRouting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Path
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] vertices = { "S", "A", "B", "C", "D" };
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                if (!Program.Iterate())
+                    break;
+            }
+
+            Dictionary<string, int> list = new Dictionary<string, int>();
+
+            foreach (var keyValue in Program.memo)
+            {
+                list.Add(keyValue.Key, keyValue.Value);
+            }
+
+            Dictionary<string, int> list1 = new Dictionary<string, int>
+            {
+                { "S", 0 },
+                { "A", 1 },
+                { "B", 5 },
+                { "C", 4 },
+                { "D", 4 },
+                { "E", 2 }
+            };
+
+            CollectionAssert.AreEqual(Program.memo, list1);
+        }
+    }
+}
